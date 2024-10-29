24b3ddb3932ed8874da87cfad8e8d5ffc22c9d35
Natanael <natancnascimento88@gmail.com>
2024-10-29 10:25:43 -0300

    modificado Troca-tudo

--- public/app.js
+++ public/app.js
@@ -2,11 +2,11 @@
 
 // Mostrar o modal do login
 function mostraModal() {
-    document.getElementById('login-modal').style.display = 'bleck';
+    document.getElementById('login-modal').style.display = 'block';
 }
 
 // fechar o modal do login
 function fecharModal() {
-    const modal = document.getElementById('login-modal');
+    const modal = document.getElementById('login-modal')
     modal.style.display = 'none'; // Centifique-se que estamos acessando o modal correto 
 }
